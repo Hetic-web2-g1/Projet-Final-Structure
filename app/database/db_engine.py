ee81784ba3476860b4f6e11d3b0e20e8b2cd5002
domino659 <martin@sionfamily.com>
2022-04-22 00:59:52 +0200

    log

--- app/database/db_engine.py
+++ app/database/db_engine.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.exc import SQLAlchemyError
+from utils.log import logging
 from utils.env import *
-from utils.log import *
 
 db_info_local = f"{db_type}://{db_user}:{db_pswd}@{db_url}/{db_name}"
 engine = create_engine(
@@ -14,6 +14,7 @@ engine = create_engine(
 
 if not database_exists(engine.url):
     create_database(engine.url)
+    logging.info("DB created")
 
 try:
     engine.connect()
@@ -31,15 +32,21 @@ from database.models.message import create_message
 
 if not engine.dialect.has_table(engine.connect(), "user"):
     create_user()
-if not engine.dialect.has_table(engine.connect(), "current_user"):
-    create_current_user()
-if not engine.dialect.has_table(engine.connect(), "subscribed_user"):
-    create_subscribed_user()
+    logging.info("Table user created")
 if not engine.dialect.has_table(engine.connect(), "field"):
-    create_field()  
+    create_field()
+    logging.info("Table field created")
 if not engine.dialect.has_table(engine.connect(), "event"):
     create_event()
+    logging.info("Table event created")
 if not engine.dialect.has_table(engine.connect(), "message"):
     create_message()
+    logging.info("Table message created")
+if not engine.dialect.has_table(engine.connect(), "current_user"):
+    create_current_user()
+    logging.info("Table current_user created")
+if not engine.dialect.has_table(engine.connect(), "subscribed_user"):
+    create_subscribed_user()
+    logging.info("Table subscribed_user created")
 
 metadata.create_all(bind=engine)
