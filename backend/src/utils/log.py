51e118f97b5211755d81c0e476a76168f41fe823
domino659 <martin@sionfamily.com>
2022-05-02 15:43:54 +0200

    ui

--- backend/src/utils/log.py
+++ backend/src/utils/log.py
@@ -11,3 +11,9 @@ logging.basicConfig(level=logging.DEBUG,
                     )
 
 logging.info("Log are activated.")
+
+# logging.debug("La fonction a bien été exécutée")
+# logging.info("Message d'information général")
+# logging.warning("Attention !")
+# logging.error("Une erreur est arrivée")
+# logging.critical("Erreur critique")
